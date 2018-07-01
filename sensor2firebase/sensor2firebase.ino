a45828e72e23dc8f49889c3f8254f62e40bb295d
EndaM <enda.molloy.1@gmail.com>
2018-07-01 22:25:50 +0100

    Update sensor2firebase.ino

--- sensor2firebase/sensor2firebase.ino
+++ sensor2firebase/sensor2firebase.ino
@@ -1,7 +1,3 @@
-
-// Example testing sketch for various DHT humidity/temperature sensors
-// Written by ladyada, public domain
-
 #include <ESP8266WiFi.h>          //ESP8266 Core WiFi Library (you most likely already have this in your sketch)
 #include <DNSServer.h>            //Local DNS Server used for redirecting all requests to the configuration portal
 #include <ESP8266WebServer.h>     //Local WebServer used to serve the configuration portal
