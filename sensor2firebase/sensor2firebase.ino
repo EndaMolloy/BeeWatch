cc7804978e584771504a91a4511a07e026ac2d47
EndaMolloy <enda.molloy.1@gmail.com>
2018-07-01 22:23:54 +0100

    read sensor and push to firebase db

---
+++ sensor2firebase/sensor2firebase.ino
@@ -0,0 +1,101 @@
+
+// Example testing sketch for various DHT humidity/temperature sensors
+// Written by ladyada, public domain
+
+#include <ESP8266WiFi.h>          //ESP8266 Core WiFi Library (you most likely already have this in your sketch)
+#include <DNSServer.h>            //Local DNS Server used for redirecting all requests to the configuration portal
+#include <ESP8266WebServer.h>     //Local WebServer used to serve the configuration portal
+#include <WiFiManager.h>          //https://github.com/tzapu/WiFiManager WiFi Configuration Magic
+
+#include <FirebaseArduino.h>
+#define FIREBASE_HOST "beewatch-d4d0a.firebaseio.com"
+//#define FIREBASE_AUTH "token_or_secret"
+#include <ArduinoJson.h>
+
+#include "DHT.h"
+#define DHTPIN 4     // what digital pin we're connected to
+#define DHTTYPE DHT11   // DHT 11
+
+
+
+// Connect pin 1 (on the left) of the sensor to +5V
+// NOTE: If using a board with 3.3V logic like an Arduino Due connect pin 1
+// to 3.3V instead of 5V!
+// Connect pin 2 of the sensor to whatever your DHTPIN is
+// Connect pin 4 (on the right) of the sensor to GROUND
+// Connect a 10K resistor from pin 2 (data) to pin 1 (power) of the sensor
+
+// Initialize DHT sensor.
+// Note that older versions of this library took an optional third parameter to
+// tweak the timings for faster processors.  This parameter is no longer needed
+// as the current DHT reading algorithm adjusts itself to work on faster procs.
+DHT dht(DHTPIN, DHTTYPE);
+
+
+void setup() {
+    // put your setup code here, to run once:
+    Serial.begin(9600);
+
+    //WiFiManager
+    //Local intialization. Once its business is done, there is no need to keep it around
+    WiFiManager wifiManager;
+    //reset saved settings
+    //wifiManager.resetSettings();
+    
+    //set custom ip for portal
+    //wifiManager.setAPStaticIPConfig(IPAddress(10,0,1,1), IPAddress(10,0,1,1), IPAddress(255,255,255,0));
+
+    //fetches ssid and pass from eeprom and tries to connect
+    //if it does not connect it starts an access point with the specified name
+    //here  "AutoConnectAP"
+    //and goes into a blocking loop awaiting configuration
+    wifiManager.autoConnect("AutoConnectAP");
+    //or use this for auto generated name ESP + ChipID
+    //wifiManager.autoConnect();
+    
+    
+    //if you get here you have connected to the WiFi
+    Serial.println("connected...yeey :)");
+    Firebase.begin(FIREBASE_HOST);
+    dht.begin();
+}
+
+void loop() {
+  // Wait a few seconds between measurements.
+  delay(2000);
+
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  Serial.print("Humidity: ");
+  Serial.print(h);
+  Serial.print(" %\t");
+  Serial.print("Temperature: ");
+  Serial.print(t);
+  Serial.print(" *C ");
+
+  StaticJsonBuffer<200> jsonBuffer;
+  JsonObject& sensorObject = jsonBuffer.createObject();
+  JsonObject& sensorTime = sensorObject.createNestedObject("timestamp");
+  sensorObject["temperature"] = t;
+  sensorObject["humidity"] = h;
+  sensorTime[".sv"] = "timestamp";
+        
+  // append a new value to /hives
+  Firebase.push("/hives/data", sensorObject);
+
+  if (Firebase.failed()) {
+    Serial.print("pushing /temperature failed:");
+    Serial.println(Firebase.error());
+    return;
+  }
+}
